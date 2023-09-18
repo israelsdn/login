f3566e0bd99343855f9519bbf8ade4bbffb3b880
Israel Sales <israelsales043@gmail.com>
2023-09-18 13:00:33 -0300

    Add cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 import { router } from './router';
+import cors from 'cors';
 require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  }),
+);
+
 app.use(router);
 
 app.listen(process.env.PORT, () => {
