a9e643d3dbfe85d2344da842a8fa80df83187914
Israel Sales <israelsales043@gmail.com>
2023-09-19 16:00:18 -0300

    teste v.8.2

--- dist/router.js
+++ dist/router.js
@@ -5,5 +5,5 @@ const express_1 = require('express');
 const usersControllers_1 = require('./controllers/usersControllers');
 exports.router = (0, express_1.Router)();
 exports.router.get('/', (req, res) => res.send('Aqui não tem nada'));
-//router.post('/user/register', registerUser);
-exports.router.post('/user/login', usersControllers_1.loginUser);
+exports.router.post('/user/register', usersControllers_1.registerUser);
+//router.post('/user/login', loginUser);
