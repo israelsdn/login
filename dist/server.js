29c2ac961f5f973aad8164f35b03a5da14b32253
Israel Sales <israelsales043@gmail.com>
2023-09-19 15:22:34 -0300

    teste v.3

--- dist/server.js
+++ dist/server.js
@@ -7,10 +7,10 @@ var __importDefault =
 Object.defineProperty(exports, '__esModule', { value: true });
 const express_1 = __importDefault(require('express'));
 const router_1 = require('./router');
+const cors_1 = __importDefault(require('cors'));
 require('dotenv').config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 app.use(router_1.router);
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+app.listen(process.env.PORT);
--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use(router);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+app.listen(process.env.PORT);
